LineageOS/android_kernel_essential_msm8998

input: vl53l0: Report SPAD count as ABS_BRAKE instead of ABS_PRESSURE
The effective SPAD count reported by the ToF sensor gets reported as an
ABS_PRESSURE value. Since Android considers anything that reports
ABS_PRESSURE to be an external stylus, it results in ridiculous pressure
values being reported whenever the ToF sensor is active (e.g. when the
camera is being accessed). Subsequently, touch events end up being
recognized as stylus inputs, which interferes with pinch-to-zoom
gestures in some applications.

To work around this, follow what Google did for wahoo and have the
kernel driver report the effective SPAD count as an ABS_BRAKE  value
instead.

Change-Id: Iebf1320248a528f16633fb196c3ba05ec8156842

drivers/input/misc/vl53L0/stmvl53l0_module.c
@@ -771,7 +771,7 @@ static void stmvl53l0_ps_read_measurement(struct stmvl53l0_data *data)
 		input_report_abs(data->input_dev_ps, ABS_WHEEL,
 				LimitCheckCurrent);
 	}
-	input_report_abs(data->input_dev_ps, ABS_PRESSURE,
+	input_report_abs(data->input_dev_ps, ABS_MISC,
 		data->rangeData.EffectiveSpadRtnCount);
 	input_sync(data->input_dev_ps);
 
@@ -2188,7 +2188,7 @@ int stmvl53l0_setup(struct stmvl53l0_data *data)
 	input_set_abs_params(data->input_dev_ps, ABS_HAT3Y, 0, 0xffffffff,
 		0, 0);
 
-	input_set_abs_params(data->input_dev_ps, ABS_PRESSURE, 0, 0xffffffff,
+	input_set_abs_params(data->input_dev_ps, ABS_MISC, 0, 0xffffffff,
 		0, 0);
 
 	input_set_abs_params(data->input_dev_ps, ABS_WHEEL , 0, 0xffffffff,

CandyDevices/kernel_essential_msm8998

To work around this, backport the change from ABS_PRESSURE to ABS_BRAKE
from driver version 1.0.5.1 so that the kernel driver will report the
effective SPAD count as an ABS_BRAKE value instead.

drivers/input/misc/vl53L0/stmvl53l0_module.c
@@ -771,7 +771,7 @@ static void stmvl53l0_ps_read_measurement(struct stmvl53l0_data *data)
 		input_report_abs(data->input_dev_ps, ABS_WHEEL,
 				LimitCheckCurrent);
 	}
-	input_report_abs(data->input_dev_ps, ABS_PRESSURE,
+	input_report_abs(data->input_dev_ps, ABS_BRAKE,
 		data->rangeData.EffectiveSpadRtnCount);
 	input_sync(data->input_dev_ps);
 
@@ -2188,7 +2188,7 @@ int stmvl53l0_setup(struct stmvl53l0_data *data)
 	input_set_abs_params(data->input_dev_ps, ABS_HAT3Y, 0, 0xffffffff,
 		0, 0);
 
-	input_set_abs_params(data->input_dev_ps, ABS_PRESSURE, 0, 0xffffffff,
+	input_set_abs_params(data->input_dev_ps, ABS_BRAKE, 0, 0xffffffff,
 		0, 0);
 
 	input_set_abs_params(data->input_dev_ps, ABS_WHEEL , 0, 0xffffffff,